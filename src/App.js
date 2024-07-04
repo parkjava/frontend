db61d4baf305141ee1281d97e44088d496e37da4
QuietMuffin89 <hgoh1210@gmail.com>
2024-07-04 16:11:16 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import './App.css';
 import logo from './logo.svg';
+import Login from './Login';
+import { Routes,Route } from 'react-router-dom';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/.js</code> and save to reload.ddd test test12312331214124412ㅇㅇㅇㅇ동민컴퓨터ddd choiyoungwonㅎㅇㅎㅇ
-          수정 테스트
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+        <Route path="/Login" element={<Login />} />
+        </Routes>
       </header>
     </div>
   );
 }
-
+export default App;
