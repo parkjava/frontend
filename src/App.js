4949fd1052fc2435e59e72479220b8d48a1eb1bf
season3689 <season3689@gmail.com>
2024-07-04 10:16:43 +0900

    ㅇㅇ

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/.js</code> and save to reload.ddd test test12312331214124412ㅇㅇㅇㅇ
+          Edit <code>src/.js</code> and save to reload.ddd test test12312331214124412ㅇㅇㅇㅇ동민컴퓨터
         </p>
         <a
           className="App-link"
