eee4d9eec30829a1cb5d6224b9afa4dce41b353b
season3689 <season3689@gmail.com>
2024-07-08 12:55:01 +0900

    공지사항작성 추가1

--- src/pages/noticePage/index.jsx
+++ src/pages/noticePage/index.jsx
@@ -1,6 +1,8 @@
 import MainNavbar from './components/noticeNavbar'
 import NoticeTable from './components/noticeTable'
 import NoticeTitle from './components/noticeTitle'
+import NoticeSearch from './components/noticeSearch'
+import NoiceCreate from './components/noticeCreate'
 
 
 export default function NoticePage() {
@@ -9,7 +11,9 @@ export default function NoticePage() {
       <div>
         <MainNavbar/>
         <NoticeTitle/>
+        <NoticeSearch/>
         <NoticeTable/>
+        <NoiceCreate/>
       </div>
     </>
   );
