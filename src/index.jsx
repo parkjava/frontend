7c669112e31f815b3f36cdeb34b5ec4d16d66ad2
Choiyoungwon0825 <fnul1005@naver.com>
2024-07-08 09:22:33 +0900

    hh

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,13 @@
-// index.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/footer'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
+      <Footer/>
   </BrowserRouter>
 );
