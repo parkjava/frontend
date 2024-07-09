b47066b2c648deac6e7f12fd75d9f4a952d23e47
QuietMuffin89 <hgoh1210@gmail.com>
2024-07-09 15:46:45 +0900

    사용자용 프로젝트 소개, 사용자 공지사항페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import MainPage from "./pages//mainPage/index";
 import LoginPage from "./pages/loginPage/index";
 import NoticePage from "./pages/noticePage/index";
@@ -6,26 +6,33 @@ import AdminPage from "./pages/adminpage/index";
 import CheckPages from "./pages/carcheckpage/index";
 import PatrolLogs from "./pages/PatrolLogPage/index";
 import "./App.css"
-
-import ControllPage from "./pages/controllPage/index";
+import ControlPage from "./pages/controllPage/index";
 import CarDetailPage from "./pages/carDetail/index"
 import PatrolDetails from "./pages/PatrolDetail";
-
+import NoticeUser from "./pages/userpage/noticepage/components/noticeUser";
+import Info from "./pages/userpage/infopage/components/projectinfo"
+import React from "react";
 
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" element={<MainPage/>}/>
-            <Route path="/notice" element={<NoticePage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/admin" element={<AdminPage/>}/>
-            <Route path="/controll" element={<ControllPage/>}/>
-            <Route path="/carcheck" element={<CheckPages/>}/>
-            <Route path="/carcheck/cardetail/1" element={<CarDetailPage/>}/>
-            <Route path="/Patrol" element={<PatrolLogs/>}/>
-            <Route path="/Patrol/PatrolDetail/1" element={<PatrolDetails/>}/>
-        </Routes>
+        <>
+            <div style={{height:'100vh'}}>
+                <Routes>
+                    <Route path="/" element={<MainPage/>}/>
+                    <Route path="/notice" element={<NoticePage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/admin" element={<AdminPage/>}/>
+                    <Route path="/controll" element={<ControlPage/>}/>
+                    <Route path="/carcheck" element={<CheckPages/>}/>
+                    <Route path="/carcheck/cardetail/1" element={<CarDetailPage/>}/>
+                    <Route path="/Patrol" element={<PatrolLogs/>}/>
+                    <Route path="/Patrol/PatrolDetail/1" element={<PatrolDetails/>}/>
+                    <Route path="/noticeUser" element={<NoticeUser/>}/>
+                    <Route path="/info" element={<Info/>}/>
+                </Routes>
+            </div>
+        </>
     );
 }
 
