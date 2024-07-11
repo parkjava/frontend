f9f91942472c8645c03f1b88db2a5cabe94e2d18
Choiyoungwon0825 <fnul1005@naver.com>
2024-07-11 17:04:04 +0900

    ㅇ

--- src/pages/adminPage/notice/detail/index.jsx
+++ src/pages/adminPage/notice/detail/index.jsx
@@ -1,14 +1,10 @@
-import MainNavbar from './components/noticeNavbar'
 import NoticeTitle from './components/noticeTitle'
 import NoticeTable from './components/noticeTable'
-
-
 
 export default function Index() {
   return (
     <>
       <div>
-        <MainNavbar/>
         <NoticeTitle/>
         <NoticeTable/>
       </div>
