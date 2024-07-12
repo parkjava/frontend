2f21f81a19eb6a16056746fa52d716a236b6d8e0
QuietMuffin89 <hgoh1210@gmail.com>
2024-07-12 10:58:52 +0900

    공지사항 작성시 쿠키에 있는 데이터 자동적용

--- src/pages/adminPage/notice/components/noticeTable.jsx
+++ src/pages/adminPage/notice/components/noticeTable.jsx
@@ -99,7 +99,7 @@ export default function NoticeTable() {
                             <Link to={`/admin/notice/${notice.noticeIndex}`}>{notice.noticeTitle}</Link>
                         </td>
                         <td>{new Date(notice.createDate).toLocaleDateString()}</td>
-                        <td>{notice.userName}</td>
+                        <td>{notice.adminName}</td>
                         <td>{notice.noticeView}</td>
                     </tr>
                 ))}
