32e361cf21c670572c2c3b466b8f62f6140e9167
QuietMuffin89 <hgoh1210@gmail.com>
2024-07-18 17:02:30 +0900

    만든이 소개 틀 , 문의 페이지 테두리 틀

--- src/pages/mainPage/creator/index.jsx
+++ src/pages/mainPage/creator/index.jsx
@@ -4,73 +4,106 @@ import zeroone from '../../../static/images/zeroone.png'
 import hyunjun from '../../../static/images/hyunjun.png'
 import dongmin from '../../../static/images/dongmin.jpg'
 import hakgyun from '../../../static/images/hakgyun.png'
+import "../../../static/common.css"
+import Test from './tespage'
 
-export default function index(){
+export default function index() {
     return <>
-        <h1>프로젝트 4조 -OOB-</h1>
-        <Card style={{width: '18rem'}}>
-            <Card.Body>
-                <Image style={{width: '16rem'}} src={zeroone}/>
-                <Card.Title>최영원</Card.Title>
-                <Card.Text>
-                    조원소개
-                </Card.Text>
-
-            </Card.Body>
-        </Card>
-        <div>
-            <p>mbti</p>
-            <p>성격</p>
-            <p>mbti</p>
-            <p>성격</p>
-        </div>
-        <Card style={{width: '18rem'}}>
-            <Card.Body>
-                <Image style={{width: '16rem', float: 'right'}} src={hyunjun}/>
-                <Card.Title>박현준</Card.Title>
-                <Card.Text>
-                    조원소개
-                </Card.Text>
-
-            </Card.Body>
-        </Card>
-        <div>
-            <p>mbti</p>
-            <p>성격</p>
-            <p>mbti</p>
-            <p>성격</p>
-        </div>
-        <Card style={{width: '18rem'}}>
-            <Card.Body>
-                <Image style={{width: '16rem'}} src={dongmin}/>
-                <Card.Title>이동민</Card.Title>
-                <Card.Text>
-                    조원소개
-                </Card.Text>
-
-            </Card.Body>
-        </Card>
-        <div>
-            <p>mbti</p>
-            <p>성격</p>
-            <p>mbti</p>
-            <p>성격</p>
-        </div>
-        <Card style={{width: '18rem'}}>
-            <Card.Body>
-                <Image style={{width: '16rem'}} src={hakgyun}/>
-                <Card.Title>오학균</Card.Title>
-                <Card.Text>
-                    조원소개
-                </Card.Text>
-
-            </Card.Body>
-        </Card>
-        <div>
-            <p>mbti</p>
-            <p>성격</p>
-            <p>mbti</p>
-            <p>성격</p>
-        </div>
+        <Test/>
     </>
 }
+    // return <div className={'creates'}>
+    //     <h1 className={'createH1'} style={{textAlign: 'center', margin: '50px 0 50px 0',fontFamily: 'MoveSans-Bold, sans-serif'}}>프로젝트 4조 -OOB-</h1>
+    //     <div className={'infoLeft'} style={{backgroundColor: '#81F781',borderRadius:'10px'}}>
+    //         <div className={'infoCard'}>
+    //             <Card>
+    //                 <Card.Body className={'card-body'} style={{backgroundColor: '#ffffff'}}>
+    //                     <Image style={{width: '16rem'}} src={hakgyun}/>
+    //                     <Card.Title style={{color: '#81F781', marginTop: '10px', fontSize: '50px'}}>오학균</Card.Title>
+    //                     <Card.Text style={{color: '#2E9AFE',fontSize: '25px'}}>
+    //                         #노친네
+    //                         #통나무
+    //                         #ㅅㅂ 정신적 지주
+    //                     </Card.Text>
+    //                 </Card.Body>
+    //             </Card>
+    //         </div>
+    //         <div className={'createInfo'}>
+    //             <p>나이 : 34(현정부나이)</p>
+    //             <p>성격 : 차분함, 긍정적</p>
+    //             <p>mbti : ISFP</p>
+    //             <p>취미 : 게임, 유튜브시청</p>
+    //             <p>담당업무 :</p>
+    //         </div>
+    //     </div>
+    //     <div className={'infoRight'} style={{backgroundColor: '#FAAC58',borderRadius:'10px'}}>
+    //         <div className={'createInfo'}>
+    //             <p>mbti</p>
+    //             <p>성격</p>
+    //             <p>mbti</p>
+    //             <p>성격</p>
+    //             <p>담당업무 :</p>
+    //         </div>
+    //         <div className={'infoCard'}>
+    //             <Card style={{width: '18rem'}}>
+    //                 <Card.Body className={'card-body'} style={{backgroundColor: '#FFFFFF'}}>
+    //                     <Image style={{width: '16rem', float: 'right'}} src={dongmin}/>
+    //                     <Card.Title style={{color: '#FAAC58', marginTop: '10px', fontSize: '50px'}}>이동민</Card.Title>
+    //                     <Card.Text style={{color: '#2E9AFE',fontSize: '25px'}}>
+    //                         #독고다이
+    //                         #안경잽이
+    //                         #혼자서도 잘해요
+    //                     </Card.Text>
+    //
+    //                 </Card.Body>
+    //             </Card>
+    //         </div>
+    //     </div>
+    //     <div className={'infoLeft'} style={{backgroundColor: '#F3F781',borderRadius:'10px'}}>
+    //         <div className={'infoCard'}>
+    //             <Card style={{width: '18rem'}}>
+    //                 <Card.Body className={'card-body'} style={{backgroundColor: '#ffffff'}}>
+    //                     <Image style={{width: '16rem'}} src={hyunjun}/>
+    //                     <Card.Title style={{color: '#F3F781', marginTop: '10px', fontSize: '50px'}}>박현준</Card.Title>
+    //                     <Card.Text style={{color: '#2E9AFE',fontSize: '25px'}}>
+    //                         #지각왕
+    //                         #긴다리
+    //                         #초슬림
+    //                     </Card.Text>
+    //
+    //                 </Card.Body>
+    //             </Card>
+    //         </div>
+    //         <div className={'createInfo'}>
+    //             <p>mbti</p>
+    //             <p>성격</p>
+    //             <p>mbti</p>
+    //             <p>성격</p>
+    //             <p>담당업무 :</p>
+    //         </div>
+    //     </div>
+    //     <div className={'infoRight'} style={{backgroundColor: '#2E64FE',borderRadius:'10px'}}>
+    //         <div className={'createInfo'}>
+    //             <p>mbti</p>
+    //             <p>성격</p>
+    //             <p>mbti</p>
+    //             <p>성격</p>
+    //             <p>담당업무 :</p>
+    //         </div>
+    //         <div className={'infoCard'}>
+    //             <Card style={{width: '18rem'}}>
+    //                 <Card.Body className={'card-body'} style={{backgroundColor: '#FFFFFF'}}>
+    //                     <Image style={{width: '16rem'}} src={zeroone}/>
+    //                     <Card.Title style={{color: '#2E64FE', marginTop: '10px', fontSize: '50px'}}>최영원</Card.Title>
+    //                     <Card.Text style={{color: '#2E9AFE',fontSize: '25px'}}>
+    //                         #조장
+    //                         #만능
+    //                         #통나무짊어진놈
+    //                     </Card.Text>
+    //
+    //                 </Card.Body>
+    //             </Card>
+    //         </div>
+    //     </div>
+    // </div>
+// }
---
+++ src/pages/mainPage/creator/tespage.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import {Card, Image} from 'react-bootstrap';
+import zeroone from '../../../static/images/zeroone.png'
+import hyunjun from '../../../static/images/hyunjun.png'
+import dongmin from '../../../static/images/dongmin.jpg'
+import hakgyun from '../../../static/images/hakgyun.png'
+import "../../../static/common.css"
+
+export default function index() {
+    return <body>
+    <div className={'wrapper'}>
+        <ul className={'team'}>
+            <li className={'teamItem'}>
+                <div className={'profile profile_green'}>
+                    <Image src={hakgyun}/>
+                    <div className={'profileContents'}>
+                        <h2>5hackgun<span>I'm a tree</span></h2>
+                        <p>Jo won so gae</p>
+                    </div>
+                </div>
+            </li>
+            <li className={'teamItem'}>
+                <div className={'profile profile_orange'}>
+                    <Image src={dongmin}/>
+                    <div className={'profileContents'}>
+                        <h2>이동민<span>독고다이</span></h2>
+                        <p>조원소개</p>
+                    </div>
+                </div>
+            </li>
+            <li className={'teamItem'}>
+                <div className={'profile profile_yellow'}>
+                    <Image src={hyunjun}/>
+                    <div className={'profileContents'}>
+                        <h2>박현준<span>키다리</span></h2>
+                        <p>조원소개</p>
+                    </div>
+                </div>
+            </li>
+            <li className={'teamItem'}>
+                <div className={'profile profile_blue'}>
+                    <Image src={zeroone}/>
+                    <div className={'profileContents'}>
+                        <h2>최영원<span>조장</span></h2>
+                        <p>조원소개</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    </body>
+
+
+}
