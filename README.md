20d7d1d095df5baf49037a18a8ee8390176f6b0d
Choiyoungwon0825 <fnul1005@naver.com>
2024-07-18 17:51:17 +0900

    파일 정리 좀 함

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# 새로운 작업시
-f1 누른 후 git pull from develop
+# ⭐️ 항상 작업 시작 전
+##  git pull from develop ( develop branch 에서 병합 후 개발 !! )
 
 # 작업 완료하고, 본인 컴퓨터에서 수정 완료되었으면 무조건 커밋&푸시
-# 풀리퀘스트 생성시 , Base : develop
 
-
-asd
 ### `npm start`< 시작 
 
 ### `npm run build` < 배포 전 과정
--- src/pages/mainPage/creator/tespage.jsx
+++ src/pages/mainPage/creator/tespage.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import {Card, Image} from 'react-bootstrap';
 import zeroone from '../../../static/images/zeroone.png'
 import hyunjun from '../../../static/images/hyunjun.png'
-import dongmin from '../../../static/images/dongmin.jpg'
+import dongmin from '../../../static/images/dongmin.png'
 import hakgyun from '../../../static/images/hakgyun.png'
 import "../../../static/common.css"
 
 export default function index() {
-    return <body>
+    return <>
     <h1>Project 4 O.O.B</h1>
     <div className={'wrapper'}>
         <ul className={'team'}>
             <li className={'teamItem'}>
                 <div className={'profile profile_green'}>
-                    <Image src={hakgyun}/>
+                    <Image src={hakgyun} style={{backgroundColor:'#47A7F9'}}/>
                     <div className={'profileContents'}>
                         <h2>5hackgun<span>I'm a tree</span></h2>
                         <p>Jo won so gae</p>
@@ -22,7 +22,7 @@ export default function index() {
             </li>
             <li className={'teamItem'}>
                 <div className={'profile profile_orange'}>
-                    <Image src={dongmin}/>
+                    <Image src={dongmin} style={{backgroundColor:"orange"}}/>
                     <div className={'profileContents'}>
                         <h2>이동민<span>독고다이</span></h2>
                         <p>조원소개</p>
@@ -31,7 +31,7 @@ export default function index() {
             </li>
             <li className={'teamItem'}>
                 <div className={'profile profile_yellow'}>
-                    <Image src={hyunjun}/>
+                    <Image src={hyunjun} style={{backgroundColor:'#ffe229'}}/>
                     <div className={'profileContents'}>
                         <h2>박현준<span>키다리</span></h2>
                         <p>조원소개</p>
@@ -40,7 +40,7 @@ export default function index() {
             </li>
             <li className={'teamItem'}>
                 <div className={'profile profile_blue'}>
-                    <Image src={zeroone}/>
+                    <Image src={zeroone} style={{backgroundColor:'#1f4dff'}}/>
                     <div className={'profileContents'}>
                         <h2>최영원<span>조장</span></h2>
                         <p>조원소개</p>
@@ -49,7 +49,7 @@ export default function index() {
             </li>
         </ul>
     </div>
-    </body>
+    </>
 
 
 }
