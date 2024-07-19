2239032a77709fcc6b35811a7f1a3886f8bfc7e9
QuietMuffin89 <hgoh1210@gmail.com>
2024-07-19 17:47:02 +0900

    만든이 css 조금 손봄, 프로젝트 소개 페이지 살짝 만듬.. ㅋㅋ

--- src/pages/mainPage/projectInfo/index.jsx
+++ src/pages/mainPage/projectInfo/index.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Image} from 'react-bootstrap';
-import summerImg from '../../../static/images/summer.jpg'
+import metalpikachu from '../../../static/images/metalpikachu.png'
+import aipikachu from '../../../static/images/aipikachu.jpg'
+import "../../../static/common.css"
 
-export default function Index(){
+export default function Index() {
+
+    const [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        setAnimate(true);
+    }, []);
+
     return (
-        <>
+        <div className={'projectInfo'}>
             <div>
-                <h1>AI인공지능 자율주행 단속로봇</h1>
-                <p>자율주행 단속로봇을 이용해 더 정확하게 단속하고 단속이 취약하던 지역까지 모두 단속이 가능합니다.</p>
+                <h1 className={'projectTitle'}>AI인공지능 자율주행 단속로봇</h1>
+                <div className={`metal_image ${animate ? 'animate' : ''}`}>
+                    <Image src={metalpikachu} alt="metalpikachu"/>
+                </div>
+                <p className={`p-info ${animate ? 'animate' : ''}`}>
+                    자율주행 단속로봇을 이용해 더 정확하게 단속하고<br/> 단속이 취약하던 지역까지 모두 단속이 가능합니다.
+                </p>
                 <hr/>
 
             </div>
-            <hr/>
+
             <div>
                 <h2>자율주행 단속로봇의 특장점</h2>
-                <p>단속 효율을 극대화 할수 있습니다.</p>
-                <hr/>
-
-                <p></p>
+                <div>
+                    <p className={`p-info2 ${animate ? 'animate' : ''}`}>단속 효율을 극대화 할수 있습니다.</p>
+                </div>
                 <div className="img1">
-                    <div className="img2">
-                        <Image src={summerImg} className="girlsImage" alt="소녀들사진"/>
+                    <div className={`img2 ${animate ? 'animate' : ''}`}>
+                        <Image src={aipikachu} className="aipikachu" alt="AI생성 피카츄"/>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
 
 
     );
