dc8d6fd3f37d8ab24b09a076a697dc8eb59149da
QuietMuffin89 <hgoh1210@gmail.com>
2024-08-07 17:13:41 +0900

    각 상세페이지 양식 통일

--- src/pages/adminPage/patrol/detail/index.jsx
+++ src/pages/adminPage/patrol/detail/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import {useParams, useNavigate, Link} from 'react-router-dom';
-import {Container, Card, Button, Col, Row} from 'react-bootstrap';
+import {Container, Card, Button, Col, Row, Table} from 'react-bootstrap';
 import axiosInstance from "../../../../common/components/axiosinstance";
 
 export default function PatrolDetail() {
@@ -19,7 +19,7 @@ export default function PatrolDetail() {
 
     useEffect(() => {
         patrolDetailApi();
-    }, []);
+    }, [patrolIndex]);
 
     const handleEdit = () => {
         navigate(`/admin/patrol/update/${patrolIndex}`);
@@ -40,25 +40,64 @@ export default function PatrolDetail() {
     }
 
     return (
+
         <div className={'commonContainer'}>
-            <Container>
-                <Card>
-                    <Card.Header>관할 구역: {patrol.patrolArea}</Card.Header>
-                    <Card.Body>
-                        <Card.Text>
+            {/*<Container>*/}
+            <Container className='detailContainer' style={{height: '100vh', borderRadius: '20px'}}>
+                <h1 className={'adminPatrolTitle'}>
+                    관할구역: {patrol.patrolArea}
+                </h1>
+                <p className={'adminPatrolDate'}>
+                    {new Date(patrol.createDate).toLocaleDateString('ko-KR')}
+                </p>
+                <Table className={'adminDetailTable'} bordered>
+                    <tbody>
+                    <tr>
+                        <td className={'adminPatrolText'} colSpan={8} style={{height: '600px'}}>
                             {patrol.patrolSummary}
-                        </Card.Text>
-                    </Card.Body>
-                </Card>
-                <Row className="mt-3">
-                    <Col md={6} className="text-md-end">
-                        <Button variant="primary" onClick={handleEdit} className="w-30">수정</Button>
-                    </Col>
-                    <Col md={6} className="text-md-start mt-2 mt-md-0">
-                        <Button variant="danger" onClick={handleDelete} className="w-30">삭제</Button>
-                    </Col>
-                </Row>
-                <div className='d-flex pt-3'><Link to={'../admin/patrol'}>목록으로</Link></div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={4} className="text-center">
+                            {/*{prevIndex >= 0 && ( // 이전 글 링크를 조건에 따라 렌더링*/}
+                            <p>
+                                <Link to={`/admin/patrol/${parseInt(patrolIndex) - 1}`}>이전 글▲</Link>
+                            </p>
+                            {/*// )}*/}
+                            {/*{nextIndex <= penalties.length && ( // 다음 글 링크를 조건에 따라 렌더링*/}
+                            <p>
+                                <Link to={`/admin/patrol/${parseInt(patrolIndex) + 1}`}>다음 글▼</Link>
+                            </p>
+                            {/*// )}*/}
+                        </td>
+                    </tr>
+
+                        {/*<td className="text-md-end">*/}
+                            <Button variant="primary" onClick={handleEdit} className="w-30">수정</Button>
+                        {/*</td>*/}
+                        {/*<td className="text-md-start mt-2 mt-md-0">*/}
+                            <Button variant="danger" onClick={handleDelete} className="w-30">삭제</Button>
+                        {/*</td>*/}
+
+                    </tbody>
+
+                </Table>
+                <div className={'noticeDetailBtn'}>
+                    <Button className='noticeListBtn'><Link
+                        to={'../admin/patrol'}>목록으로</Link></Button>
+
+                </div>
+                {/*</Container>*/}
+                {/*<Card>*/}
+                {/*    <Card.Header>관할 구역: {patrol.patrolArea}</Card.Header>*/}
+                {/*    <Card.Body>*/}
+                {/*        <Card.Text>*/}
+                {/*            {patrol.patrolSummary}*/}
+                {/*        </Card.Text>*/}
+                {/*    </Card.Body>*/}
+                {/*</Card>*/}
+
+                {/*        <div className='d-flex pt-3'><Link to={'../admin/patrol'}>목록으로</Link></div>*/}
             </Container>
         </div>
     );
